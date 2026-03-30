de207623c16b6d9ddc9cab525abe75a9761c6fa4
Sourav Mukherjee <146521158+mukherjeesourav687@users.noreply.github.com>
2026-03-30 15:20:44 +0530

    Add structured SQL scripts for retail analytics (cleaning, correction, transformation, EDA, customer analysis, segmentation)

---
+++ 00_master_script.sql
@@ -0,0 +1,10 @@
+-- Retail Analytics Project - Master Script
+
+CREATE DATABASE IF NOT EXISTS retail;
+USE retail;
+
+-- Rename tables (after import)
+ALTER TABLE `customer_profiles-1-1714027410` RENAME TO customer_profiles;
+ALTER TABLE `product_inventory-1-1714027438` RENAME TO product_inventory;
+ALTER TABLE `sales_transaction-1714027462` RENAME TO sales_transaction;
+
